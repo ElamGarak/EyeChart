ac8c7bccc6694844430b37066d158e88cbe817e4
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-15 14:46:10 -0700

    Added method

--- module/EyeChart/src/DAO/Authenticate/AuthenticateDAO.php
+++ module/EyeChart/src/DAO/Authenticate/AuthenticateDAO.php
@@ -9,10 +9,14 @@ declare(strict_types=1);
 
 namespace EyeChart\DAO\Authenticate;
 
+use EyeChart\Mappers\AuthenticateMapper;
 use EyeChart\VO\AuthenticationVO;
 use EyeChart\VO\VOInterface;
 use Zend\Db\Adapter\Adapter;
+use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Where;
 
 /**
  * Class AuthenticateDAO
@@ -33,12 +37,36 @@ class AuthenticateDAO
     }
 
     /**
-     * TODO
      * @param AuthenticationVO|VOInterface $vo
      * @return bool
      */
     public function checkCredentials(VOInterface $vo): bool
     {
+        $select = $this->sql->select();
+
+        $select->columns([
+            AuthenticateMapper::USER_NAME,
+        ])->from(AuthenticateMapper::TABLE);
+
+        $where = new Where();
+        $where->equalTo(AuthenticateMapper::USER_NAME, $vo->getUsername())->and
+              ->equalTo(AuthenticateMapper::PASSWORD, $vo->getPassword());
+
+        $select->where($where);
+
+        $statement = $this->sql->prepareStatementForSqlObject($select);
+
+        $result = $statement->execute();
+
+        if ($result instanceof ResultInterface && $result->isQueryResult()) {
+            $resultSet = new ResultSet(ResultSet::TYPE_ARRAY);
+            $resultSet->initialize($result);
+
+            $row = $resultSet->current();
+
+            return (! is_null($row));
+        }
+
         return false;
     }
 
