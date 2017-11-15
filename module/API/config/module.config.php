364fe62ecdad90a83b5ce67283b230fa45d5e21f
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-15 07:57:22 -0700

    Added login validation

--- module/API/config/module.config.php
+++ module/API/config/module.config.php
@@ -1,1 +1,191 @@
-<?phpreturn [    'controllers' => [        'factories' => [            'API\\V1\\Rpc\\Login\\Controller' => \API\V1\Rpc\Login\LoginControllerFactory::class,            'API\\V1\\Rpc\\Logout\\Controller' => \API\V1\Rpc\Logout\LogoutControllerFactory::class,            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => \API\V1\Rpc\CheckSessionStatus\CheckSessionStatusControllerFactory::class,            'API\\V1\\Rpc\\RefreshSession\\Controller' => \API\V1\Rpc\RefreshSession\RefreshSessionControllerFactory::class,        ],    ],    'router' => [        'routes' => [            'api.rpc.login' => [                'type' => 'Segment',                'options' => [                    'route' => '/api/login',                    'defaults' => [                        'controller' => 'API\\V1\\Rpc\\Login\\Controller',                        'action' => 'login',                    ],                ],            ],            'api.rpc.logout' => [                'type' => 'Segment',                'options' => [                    'route' => '/api/logout',                    'defaults' => [                        'controller' => 'API\\V1\\Rpc\\Logout\\Controller',                        'action' => 'logout',                    ],                ],            ],            'api.rpc.edit-application-type' => [                'type' => 'Segment',                'options' => [                    'route' => '/api/editApplicationType',                    'defaults' => [                        'controller' => 'API\\V1\\Rpc\\EditApplicationType\\Controller',                        'action' => 'editApplicationType',                    ],                ],            ],            'api.rpc.check-session-status' => [                'type' => 'Segment',                'options' => [                    'route' => '/api/checkSessionStatus',                    'defaults' => [                        'controller' => 'API\\V1\\Rpc\\CheckSessionStatus\\Controller',                        'action' => 'checkSessionStatus',                    ],                ],            ],            'api.rpc.refresh-session' => [                'type' => 'Segment',                'options' => [                    'route' => '/api/refreshSession',                    'defaults' => [                        'controller' => 'API\\V1\\Rpc\\RefreshSession\\Controller',                        'action' => 'refreshSession',                    ],                ],            ],        ],    ],    'zf-versioning' => [        'uri' => [            0 => 'api.rpc.login',            1 => 'api.rpc.logout',            2 => 'api.rpc.check-session-status',            3 => 'api.rpc.refresh-session',        ],    ],    'zf-rpc' => [        'API\\V1\\Rpc\\Login\\Controller' => [            'service_name' => 'Login',            'http_methods' => [                0 => 'POST',            ],            'route_name' => 'api.rpc.login',        ],        'API\\V1\\Rpc\\Logout\\Controller' => [            'service_name' => 'Logout',            'http_methods' => [                0 => 'POST',            ],            'route_name' => 'api.rpc.logout',        ],        'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => [            'service_name' => 'CheckSessionStatus',            'http_methods' => [                0 => 'POST',            ],            'route_name' => 'api.rpc.check-session-status',        ],        'API\\V1\\Rpc\\RefreshSession\\Controller' => [            'service_name' => 'RefreshSession',            'http_methods' => [                0 => 'POST',            ],            'route_name' => 'api.rpc.refresh-session',        ],    ],    'zf-content-negotiation' => [        'controllers' => [            'API\\V1\\Rpc\\Login\\Controller' => 'Json',            'API\\V1\\Rpc\\Logout\\Controller' => 'Json',            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => 'Json',        ],        'accept_whitelist' => [            'API\\V1\\Rpc\\Login\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',                2 => 'application/*+json',            ],            'API\\V1\\Rpc\\Logout\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',                2 => 'application/*+json',            ],            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',                2 => 'application/*+json',            ],            'API\\V1\\Rpc\\RefreshSession\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',                2 => 'application/*+json',            ],        ],        'content_type_whitelist' => [            'API\\V1\\Rpc\\Login\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',            ],            'API\\V1\\Rpc\\Logout\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',            ],            'API\\V1\\Rpc\\RefreshSession\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',            ],            'API\\V1\\Rpc\\GetFuelCard\\Controller' => [                0 => 'application/vnd.api.v1+json',                1 => 'application/json',            ],        ],    ],    'zf-mvc-auth' => [        'authorization' => [        ],    ],    'zf-content-validation' => [        'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => [            'input_filter' => 'API\\V1\\Rpc\\CheckSessionStatus\\Validator',        ],    ],    'input_filter_specs' => [        'API\\V1\\Rpc\\CheckSessionStatus\\Validator' => [            0 => [                'required' => true,                'validators' => [],                'filters' => [],                'name' => 'token',                'field_type' => 'string',                'allow_empty' => true,                'continue_if_empty' => true,            ],        ],    ],];
+<?php
+return array(
+    'controllers' => array(
+        'factories' => array(
+            'API\\V1\\Rpc\\Login\\Controller' => 'API\\V1\\Rpc\\Login\\LoginControllerFactory',
+            'API\\V1\\Rpc\\Logout\\Controller' => 'API\\V1\\Rpc\\Logout\\LogoutControllerFactory',
+            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => 'API\\V1\\Rpc\\CheckSessionStatus\\CheckSessionStatusControllerFactory',
+            'API\\V1\\Rpc\\RefreshSession\\Controller' => 'API\\V1\\Rpc\\RefreshSession\\RefreshSessionControllerFactory',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'api.rpc.login' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/login',
+                    'defaults' => array(
+                        'controller' => 'API\\V1\\Rpc\\Login\\Controller',
+                        'action' => 'login',
+                    ),
+                ),
+            ),
+            'api.rpc.logout' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/logout',
+                    'defaults' => array(
+                        'controller' => 'API\\V1\\Rpc\\Logout\\Controller',
+                        'action' => 'logout',
+                    ),
+                ),
+            ),
+            'api.rpc.edit-application-type' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/editApplicationType',
+                    'defaults' => array(
+                        'controller' => 'API\\V1\\Rpc\\EditApplicationType\\Controller',
+                        'action' => 'editApplicationType',
+                    ),
+                ),
+            ),
+            'api.rpc.check-session-status' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/checkSessionStatus',
+                    'defaults' => array(
+                        'controller' => 'API\\V1\\Rpc\\CheckSessionStatus\\Controller',
+                        'action' => 'checkSessionStatus',
+                    ),
+                ),
+            ),
+            'api.rpc.refresh-session' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api/refreshSession',
+                    'defaults' => array(
+                        'controller' => 'API\\V1\\Rpc\\RefreshSession\\Controller',
+                        'action' => 'refreshSession',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'zf-versioning' => array(
+        'uri' => array(
+            0 => 'api.rpc.login',
+            1 => 'api.rpc.logout',
+            2 => 'api.rpc.check-session-status',
+            3 => 'api.rpc.refresh-session',
+        ),
+    ),
+    'zf-rpc' => array(
+        'API\\V1\\Rpc\\Login\\Controller' => array(
+            'service_name' => 'Login',
+            'http_methods' => array(
+                0 => 'POST',
+            ),
+            'route_name' => 'api.rpc.login',
+        ),
+        'API\\V1\\Rpc\\Logout\\Controller' => array(
+            'service_name' => 'Logout',
+            'http_methods' => array(
+                0 => 'POST',
+            ),
+            'route_name' => 'api.rpc.logout',
+        ),
+        'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => array(
+            'service_name' => 'CheckSessionStatus',
+            'http_methods' => array(
+                0 => 'POST',
+            ),
+            'route_name' => 'api.rpc.check-session-status',
+        ),
+        'API\\V1\\Rpc\\RefreshSession\\Controller' => array(
+            'service_name' => 'RefreshSession',
+            'http_methods' => array(
+                0 => 'POST',
+            ),
+            'route_name' => 'api.rpc.refresh-session',
+        ),
+    ),
+    'zf-content-negotiation' => array(
+        'controllers' => array(
+            'API\\V1\\Rpc\\Login\\Controller' => 'Json',
+            'API\\V1\\Rpc\\Logout\\Controller' => 'Json',
+            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => 'Json',
+        ),
+        'accept_whitelist' => array(
+            'API\\V1\\Rpc\\Login\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+            'API\\V1\\Rpc\\Logout\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+            'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+            'API\\V1\\Rpc\\RefreshSession\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+                2 => 'application/*+json',
+            ),
+        ),
+        'content_type_whitelist' => array(
+            'API\\V1\\Rpc\\Login\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+            ),
+            'API\\V1\\Rpc\\Logout\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+            ),
+            'API\\V1\\Rpc\\RefreshSession\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+            ),
+            'API\\V1\\Rpc\\GetFuelCard\\Controller' => array(
+                0 => 'application/vnd.api.v1+json',
+                1 => 'application/json',
+            ),
+        ),
+    ),
+    'zf-mvc-auth' => array(
+        'authorization' => array(),
+    ),
+    'zf-content-validation' => array(
+        'API\\V1\\Rpc\\CheckSessionStatus\\Controller' => array(
+            'input_filter' => 'API\\V1\\Rpc\\CheckSessionStatus\\Validator',
+        ),
+        'API\\V1\\Rpc\\Login\\Controller' => array(
+            'input_filter' => 'API\\V1\\Rpc\\Login\\Validator',
+        ),
+    ),
+    'input_filter_specs' => array(
+        'API\\V1\\Rpc\\CheckSessionStatus\\Validator' => array(
+            0 => array(
+                'required' => true,
+                'validators' => array(),
+                'filters' => array(),
+                'name' => 'token',
+                'field_type' => 'string',
+                'allow_empty' => true,
+                'continue_if_empty' => true,
+            ),
+        ),
+        'API\\V1\\Rpc\\Login\\Validator' => array(
+            0 => array(
+                'required' => true,
+                'validators' => array(),
+                'filters' => array(),
+                'name' => 'username',
+                'field_type' => 'string',
+                'error_message' => 'User Name is Required',
+            ),
+            1 => array(
+                'required' => true,
+                'validators' => array(),
+                'filters' => array(),
+                'name' => 'password',
+                'error_message' => 'Password is Required',
+            ),
+        ),
+    ),
+);
