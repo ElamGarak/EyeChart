926882b74f4b23128a12b5c78ffadb34d80c4958
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-17 17:45:48 -0700

    Modified to point to home page

--- module/EyeChart/config/module.config.php
+++ module/EyeChart/config/module.config.php
@@ -88,8 +88,8 @@ return [
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => [
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'layout/layout' => __DIR__ . '/../view/eye-chart/index/index.phtml',
+            'eyechart/index/index' => __DIR__ . '/../view/eye-chart/index/index.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml'
         ],
