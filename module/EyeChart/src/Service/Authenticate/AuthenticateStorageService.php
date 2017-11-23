79aa6e8b6d0a5537241ac9e93ac150189867aed8
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-22 18:43:39 -0700

    Corrected annotation

--- module/EyeChart/src/Service/Authenticate/AuthenticateStorageService.php
+++ module/EyeChart/src/Service/Authenticate/AuthenticateStorageService.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 
 namespace EyeChart\Service\Authenticate;
 
+use EyeChart\Entity\SessionEntity;
 use EyeChart\Repository\Authentication\AuthenticationRepository;
-use EyeChart\VO\TokenVO;
 use EyeChart\VO\VOInterface;
 use Zend\Authentication\Storage\StorageInterface;
 
@@ -49,12 +49,12 @@ final class AuthenticateStorageService implements StorageInterface
     }
 
     /**
-     * @param mixed[] $storage
+     * @param SessionEntity[] $sessionEntity
      * @return bool
      */
-    public function write($storage): bool
+    public function write($sessionEntity): bool
     {
-        return $this->authenticationRepository->write($storage);
+        return $this->authenticationRepository->write($sessionEntity);
     }
 
     public function clear(): void
@@ -63,19 +63,11 @@ final class AuthenticateStorageService implements StorageInterface
     }
 
     /**
-     * @return mixed[]
-     */
-    public function getEmployeeInformation()
-    {
-        return $this->authenticationRepository->getEmployeeInformation();
-    }
-
-    /**
-     * @param VOInterface|TokenVO $tokenVO
+     * @param VOInterface $tokenVO
      * @return array
      */
-    public function getTokenSession(TokenVO $tokenVO): array
+    public function getUserSessionByToken(VOInterface $tokenVO): array
     {
-        return $this->authenticationRepository->getTokenSession($tokenVO);
+        return $this->authenticationRepository->getUserSessionStatus($tokenVO);
     }
 }
