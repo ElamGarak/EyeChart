ab05a39b5fa9d9426cf50750c28436887b87d383
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-24 12:09:21 -0700

    Initial Commit

---
+++ module/EyeChart/src/Exception/InvalidPostRequestException.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Joshua M Pacheco <joshua.pacheco@gmail.com>
+ * Date: 11/24/2017
+ * (c) 2017
+ */
+
+namespace EyeChart\Exception;
+
+use HttpInvalidParamException;
+use Throwable;
+
+/**
+ * Class InvalidPostRequestException
+ * @package EyeChart\Exception
+ */
+class InvalidPostRequestException extends HttpInvalidParamException
+{
+    protected $message = "Post key does not exist";
+
+    /**
+     * InvalidHeaderRequestException constructor.
+     * @param string $key
+     * @param int $code
+     * @param Throwable $previous
+     */
+    public function __construct(string $key = '', int $code = 500, Throwable $previous)
+    {
+        if (!empty($key)) {
+            parent::__construct($this->message, $code, $previous);
+
+            return;
+        }
+
+        parent::__construct("{$key} does not exist within incoming post payload");
+    }
+}
