48947b6432a1ca05fd90dd4cda16cfd19bad2612
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-24 12:36:10 -0700

    Abstracted controller and simplified

--- module/EyeChart/src/Controller/Index/IndexController.php
+++ module/EyeChart/src/Controller/Index/IndexController.php
@@ -9,37 +9,17 @@ declare(strict_types=1);
 
 namespace EyeChart\Controller\Index;
 
-use EyeChart\Entity\AuthenticateEntity;
+use EyeChart\Controller\AbstractController;
+use EyeChart\Mappers\AuthenticateMapper;
+use EyeChart\Mappers\MenuMapper;
 use Zend\View\Model\ViewModel;
-use Zend\Mvc\Controller\AbstractActionController;
-use League\Tactician\CommandBus;
-use EyeChart\Command\Commands\AuthenticateCommand;
 
 /**
  * Class IndexController
  * @package EyeChart\Controller\Index
  */
-final class IndexController extends AbstractActionController
+final class IndexController extends AbstractController
 {
-
-    /** @var AuthenticateEntity */
-    private $authenticateEntity;
-
-    /** @var CommandBus */
-    private $commandBus;
-
-    /**
-     * Constructor
-     *
-     * @param AuthenticateEntity $authenticateEntity
-     * @param CommandBus         $commandBus
-     */
-    public function __construct(AuthenticateEntity $authenticateEntity, CommandBus $commandBus)
-    {
-        $this->authenticateEntity = $authenticateEntity;
-        $this->commandBus         = $commandBus;
-    }
-
     /**
      * @return ViewModel
      */
@@ -47,28 +27,9 @@ final class IndexController extends AbstractActionController
     {
         $this->authenticate();
 
-        $this->layout()->setVariable('token', $this->authenticateEntity->getToken());
-        $this->layout()->setVariable('menuRoute', $this->getEvent()
-                                                       ->getRouteMatch()
-                                                       ->getMatchedRouteName());
+        $this->layout()->setVariable(AuthenticateMapper::TOKEN, parent::getAuthenticateEntity()->getToken());
+        $this->layout()->setVariable(MenuMapper::MENU_ROUTE, $this->getMatchedRouteName());
 
         return new ViewModel();
-    }
-
-    private function authenticate(): void
-    {
-        // TODO This will be addressed in issue #2
-
-        return;
-
-        try {
-            $this->commandBus->handle(
-                new AuthenticateCommand(
-                    $this->getEvent()
-                )
-            );
-        } catch (\Exception $e) {
-            $this->redirect()->toRoute('login');
-        }
     }
 }
