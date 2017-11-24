96378c591f522347de543fceb9617e4f14a739ff
Pacheco <joshua_pacheco@swifttrans.com>
2017-11-24 12:29:10 -0700

    Initial Commit

---
+++ module/EyeChart/src/Mappers/MenuMapper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Joshua M Pacheco <joshua.pacheco@gmail.com>
+ * Date: 11/24/2017
+ * (c) 2017
+ */
+
+namespace EyeChart\Mappers;
+/**
+ * Class MenuMapper
+ * @package EyeChart\Mappers
+ */
+final class MenuMapper extends AbstractMapper
+{
+    const MENU_ROUTE = 'menuRoute';
+}
