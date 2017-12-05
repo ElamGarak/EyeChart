68749ff801d142b5f5b008079166b4a2cf8dc792
Pacheco <joshua_pacheco@swifttrans.com>
2017-12-04 17:45:19 -0700

    Added test

--- tests/EyeChart/Tests/Model/Authenticate/AuthenticateModelTest.php
+++ tests/EyeChart/Tests/Model/Authenticate/AuthenticateModelTest.php
@@ -12,6 +12,7 @@ namespace API\Tests\Model\Authenticate;
 use EyeChart\DAO\Authenticate\AuthenticateDAO;
 use EyeChart\Entity\AuthenticateEntity;
 use EyeChart\Entity\SessionEntity;
+use EyeChart\Exception\UserCredentialsDoNotMatchException;
 use EyeChart\Mappers\AuthenticateMapper;
 use EyeChart\Model\Authenticate\AuthenticateModel;
 use EyeChart\VO\Authentication\AuthenticationVO;
@@ -36,6 +37,19 @@ final class AuthenticateModelTest extends TestCase
 
     /** @var AuthenticateModel */
     private $model;
+
+    /** @var string */
+    private static $validCode = '';
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$validCode  = 'def10000def50200279ce24cccc729b59d342226b09a27aea8a19b1d8ee63b9bd556f3c90746b9a54b7b91a0f';
+        self::$validCode .= '4e6c84ca0e5f32b1788513b22f1f0f8b1a537ee7873b14bb72151ff082703818ff174eff1dbd8ee80ccff634e';
+        self::$validCode .= 'e21db37c23d42da171552e427329aa755b0cb3053fff4a9d5a253e06f7ec2da0048d41f10ce91f56e4ce4024a';
+        self::$validCode .= '58d3f65a57ad9a214b27bd9585ce0dbae044d16676828ff046c8a1b3bfdf8f1842ba3d6edf03fd207ac18d039';
+        self::$validCode .= '414668862f104ca9b33277e4e147856046b3bcdfdc20f07d25ddb0318bb98d8ca93ee6076344e077a5ef77e5a';
+        self::$validCode .= 'dcd6ee24ad4a73dfcf158b57cfabcc8fc72d274850c8a84acfac3e9aaf3f4a74534';
+    }
 
     public function setUp(): void
     {
@@ -67,24 +81,62 @@ final class AuthenticateModelTest extends TestCase
         $this->model->checkCredentials($vo);
     }
 
+    /**
+     * @expectedException \EyeChart\Exception\UnableToAuthenticateException
+     */
     public function testCheckCredentialsThrowsUnableToAuthenticateExceptionOnCryptoException(): void
     {
-        $this->markTestIncomplete();
+        $vo = AuthenticationVO::build()->setDerivedCredentials(
+            CredentialsVO::build()->setCredentials(str_repeat('1', 512))
+        );
+
+        $vo->setStoredCredentials(
+            CredentialsVO::build()->setCredentials(str_repeat('1', 512))
+        );
+
+        $this->model->checkCredentials($vo);
     }
 
+    /**
+     * @expectedException \EyeChart\Exception\UnableToAuthenticateException
+     */
     public function testCheckCredentialsThrowsUnableToAuthenticateExceptionOnUserCredentialsDoNotMatchException(): void
     {
-        $this->markTestIncomplete();
-    }
+        $vo = AuthenticationVO::build()->setDerivedCredentials(
+            CredentialsVO::build()->setCredentials(self::$validCode)
+        );
 
-    public function checkCredentialsSetsAuthenticateEntityEntityTrue(): void
-    {
-        $this->markTestIncomplete();
+        $vo->setStoredCredentials(
+            CredentialsVO::build()->setCredentials(self::$validCode)
+        );
+
+        $vo->setPassword('foo');
+
+        $this->mockedDao->expects($this->once())
+             ->method('checkCredentials')
+             ->willThrowException(new UserCredentialsDoNotMatchException());
+
+        $this->model->checkCredentials($vo);
     }
 
     public function testCheckCredentialsSetsAuthenticateEntityEntityTrue(): void
     {
-        $this->markTestIncomplete();
+        $vo = AuthenticationVO::build()->setDerivedCredentials(
+            CredentialsVO::build()->setCredentials(self::$validCode)
+        );
+
+        $vo->setStoredCredentials(
+            CredentialsVO::build()->setCredentials(self::$validCode)
+        );
+
+        $vo->setPassword('foo');
+
+        $this->mockedDao->expects($this->once())
+            ->method('checkCredentials');
+
+        $this->model->checkCredentials($vo);
+
+        $this->assertTrue($this->authenticateEntity->getIsValid());
     }
 
     /**
