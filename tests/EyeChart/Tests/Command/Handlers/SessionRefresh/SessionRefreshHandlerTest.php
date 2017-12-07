2305cee40f13ad513e1f33075db28d0629f6c264
Pacheco <joshua_pacheco@swifttrans.com>
2017-12-07 12:54:28 -0700

    Initial commit

---
+++ tests/EyeChart/Tests/Command/Handlers/SessionRefresh/SessionRefreshHandlerTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Josh Pacheco <joshua.pacheco@gmail.com>
+ * Date: 12/7/2017
+ * (c) Eye Chart
+ */
+
+namespace EyeChart\Tests\Command\Handlers\SessionRefresh;
+
+use EyeChart\Command\Commands\SessionRefreshCommand;
+use EyeChart\Command\Handlers\Session\SessionRefreshHandler;
+use EyeChart\Model\Authenticate\AuthenticateStorageModel;
+use EyeChart\VO\Authentication\AuthenticationVO;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
+
+/**
+ * Class SessionRefreshHandlerTest
+ * @package EyeChart\Tests\Command\Handlers\SessionRefresh
+ */
+class SessionRefreshHandlerTest extends TestCase
+{
+    /** @var SessionRefreshHandler */
+    private $handler;
+
+    /** @var AuthenticateStorageModel|PHPUnit_Framework_MockObject_MockObject */
+    private $mockedModel;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockedModel = $this->getMockBuilder(AuthenticateStorageModel::class)
+                                  ->disableOriginalConstructor()
+                                  ->getMock();
+
+        $this->handler = new SessionRefreshHandler($this->mockedModel);
+    }
+
+    public function testHandle(): void
+    {
+        $vo = AuthenticationVO::build();
+
+        $command = new SessionRefreshCommand($vo);
+
+        $this->mockedModel->expects($this->once())
+                      ->method('refresh')
+                      ->with($vo->getToken());
+
+        $this->handler->handle($command);
+    }
+}
