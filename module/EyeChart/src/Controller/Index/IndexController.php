ed02ba8f84b6ff741f2fa78509af8cc4ae70b645
Pacheco <joshua_pacheco@swifttrans.com>
2017-12-09 18:41:46 -0700

    Removed session id storage and collection

--- module/EyeChart/src/Controller/Index/IndexController.php
+++ module/EyeChart/src/Controller/Index/IndexController.php
@@ -9,17 +9,38 @@ declare(strict_types=1);
 
 namespace EyeChart\Controller\Index;
 
-use EyeChart\Controller\AbstractController;
+use EyeChart\Command\Commands\AuthenticateCommand;
+use EyeChart\Entity\AuthenticateEntity;
 use EyeChart\Mappers\AuthenticateMapper;
 use EyeChart\Mappers\MenuMapper;
+use League\Tactician\CommandBus;
+use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 /**
  * Class IndexController
  * @package EyeChart\Controller\Index
  */
-final class IndexController extends AbstractController
+final class IndexController extends AbstractActionController
 {
+    /** @var AuthenticateEntity */
+    private $authenticateEntity;
+
+    /** @var CommandBus */
+    private $commandBus;
+
+    /**
+     * Constructor
+     *
+     * @param AuthenticateEntity $authenticateEntity
+     * @param CommandBus         $commandBus
+     */
+    public function __construct(AuthenticateEntity $authenticateEntity, CommandBus $commandBus)
+    {
+        $this->authenticateEntity = $authenticateEntity;
+        $this->commandBus         = $commandBus;
+    }
+
     /**
      * @return ViewModel
      */
@@ -27,9 +48,20 @@ final class IndexController extends AbstractController
     {
         $this->authenticate();
 
-        $this->layout()->setVariable(AuthenticateMapper::TOKEN, parent::getAuthenticateEntity()->getToken());
-        $this->layout()->setVariable(MenuMapper::MENU_ROUTE, $this->getMatchedRouteName());
+        $this->layout()->setVariable(AuthenticateMapper::TOKEN, $this->authenticateEntity->getToken());
+        $this->layout()->setVariable(MenuMapper::MENU_ROUTE, $this->getEvent()->getRouteMatch()->getMatchedRouteName());
 
         return new ViewModel();
+    }
+
+    public function authenticate(): void
+    {
+        try {
+            $this->commandBus->handle(
+                new AuthenticateCommand($this->getEvent())
+            );
+        } catch (\Exception $e) {
+            $this->redirect()->toRoute('login');
+        }
     }
 }
