6a93ac5be4bd10cd9b1a781283ceed9fecca82bd
Pacheco <joshua_pacheco@swifttrans.com>
2017-12-10 18:38:48 -0700

    Updated session lifetime on db

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -38,10 +38,8 @@ return [
     ],
     // Session configuration
     'session_config' => [
-        // Session cookie will expire in 1 hour.
         'cookie_lifetime' => 60*60*1,
-        // Session data will be stored on server maximum for 30 days.
-        'gc_maxlifetime'     => 60*60*24*30,
+        'gc_maxlifetime'  => 60*60*12,
     ],
     // Session storage configuration.
     'session_storage' => [
